c0ce4b2ab711c0bc12a8209f1f17395623f0fe2d
diyan santoso <diyan3526@gmail.com>
2021-10-13 09:43:39 +0000

    Update install.sh

--- diyan/install.sh
+++ diyan/install.sh
@@ -7,13 +7,13 @@ rm -rf /usr/bin/diyan
 rm -rf /usr/bin/openwrt
 wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/diyan" -P /usr/bin &> /dev/null
 chmod 777 /usr/bin/diyan &> /dev/null
-wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/openwrt" -P /usr/bin &> /dev/null
-chmod 777 /usr/bin/openwrt &> /dev/null
+wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/openwrt" -P /usr/bin &> 2>/dev/null
+chmod 777 /usr/bin/openwrt &> 2>/dev/null
 else
 wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/diyan" -P /usr/bin &> /dev/null
 chmod 777 /usr/bin/diyan &> /dev/null
-wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/openwrt" -P /usr/bin &> /dev/null
-chmod 777 /usr/bin/openwrt &> /dev/null
+wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/openwrt" -P /usr/bin &> 2>/dev/null
+chmod 777 /usr/bin/openwrt &> 2>/dev/null
 fi
 sleep 2
 echo " diyan berhasil di install"
