3c3cbd5488055ed1b4b1f1d5c43dd033aa88faa7
diyan santoso <diyan3526@gmail.com>
2021-10-13 08:36:22 +0000

    Update install.sh

--- diyan/install.sh
+++ diyan/install.sh
@@ -4,16 +4,16 @@ sleep 2
 an=$(find /usr/bin)
 if [[ $an ==  *"diyan"* ]]; then
 rm -rf /usr/bin/diyan
-wget --no-check-certificate "https://raw.githubusercontent.com/vitoharhari/ain/main/ain" -P /usr/bin &> /dev/null
-chmod 777 /usr/bin/ain &> /dev/null
+wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/diyan" -P /usr/bin &> /dev/null
+chmod 777 /usr/bin/diyan &> /dev/null
 else
-wget --no-check-certificate "https://raw.githubusercontent.com/vitoharhari/ain/main/ain" -P /usr/bin &> /dev/null
-chmod 777 /usr/bin/ain &> /dev/null
+wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/diyan" -P /usr/bin &> /dev/null
+chmod 777 /usr/bin/diyan &> /dev/null
 fi
 sleep 2
-echo " BASE berhasil di install"
+echo " diyan berhasil di install"
 sleep 2
-echo " ketik base pada terminal untuk menjalankan tools BASE"
+echo " ketik base pada terminal untuk menjalankan tools diyan"
 sleep 2
 echo " terima kasih dan sampai jumpa di auto installer selanjutnya"
 sleep 2
