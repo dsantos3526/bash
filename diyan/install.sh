5570ae7283f05250618ac00cf77c080414704d36
diyan santoso <diyan3526@gmail.com>
2021-10-13 09:30:28 +0000

    Update install.sh

--- diyan/install.sh
+++ diyan/install.sh
@@ -4,10 +4,15 @@ sleep 2
 an=$(find /usr/bin)
 if [[ $an ==  *"diyan"* ]]; then
 rm -rf /usr/bin/diyan
+rm -rf /usr/bin/openwrt
 wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/diyan" -P /usr/bin &> /dev/null
+chmod 777 /usr/bin/diyan &> /dev/null
+wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/openwrt" -P /usr/bin &> /dev/null
 chmod 777 /usr/bin/diyan &> /dev/null
 else
 wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/diyan" -P /usr/bin &> /dev/null
+chmod 777 /usr/bin/diyan &> /dev/null
+wget --no-check-certificate "https://raw.githubusercontent.com/dsantos3526/bash/main/diyan/openwrt" -P /usr/bin &> /dev/null
 chmod 777 /usr/bin/diyan &> /dev/null
 fi
 sleep 2
